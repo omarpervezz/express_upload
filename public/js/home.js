cd70a95b965b79779ad890273840dfd6943cc285
Omar Pervez <70445442+omarpervezz@users.noreply.github.com>
2023-02-27 01:13:56 +0600

    done the work

--- public/js/home.js
+++ public/js/home.js
@@ -1,23 +1,32 @@
-// const blogSection = document.querySelector(".blogs-section");
+const blogSection = document.querySelector(".recently_listed");
 
-// db.collection("blogs")
-//   .get()
-//   .then((blogs) => {
-//     blogs.forEach((blog) => {
-//       if (blog.id != decodeURI(location.pathname.split("/").pop())) {
-//         createBlog(blog);
-//       }
-//     });
-//   });
+db.collection("blogs")
+  .get()
+  .then((blogs) => {
+    blogs.forEach((blog) => {
+      if (blog.id != decodeURI(location.pathname.split("/").pop())) {
+        createBlog(blog);
+      }
+    });
+  });
 
-// const createBlog = (blog) => {
-//   let data = blog.data();
-//   blogSection.innerHTML += `
-//     <div class="blog-card">
-//         <img src="${data.bannerImage}" class="blog-image" alt="">
-//         <h1 class="blog-title">${data.title.substring(0, 100) + "..."}</h1>
-//         <p class="blog-overview">${data.article.substring(0, 200) + "..."}</p>
-//         <a href="/${blog.id}" class="btn dark">Details</a>
-//     </div>
-//     `;
-// };
+const createBlog = (blog) => {
+  let data = blog.data();
+  blogSection.innerHTML += `
+  <a href="/${blog.id}" class="gallery__link">
+  <figure class="gallery__thumb">
+    <img
+      src="${data.bannerImage}"
+      alt="gallery_img"
+      class="gallery__image"
+    />
+    <figcaption class="gallery__caption d-flex justify-content-around">
+      <span>$${data.property_price}</span>
+      <span>${data.beds} beds</span>
+      <span>${data.baths} baths</span>
+    </figcaption>
+  </figure>
+</a>;
+
+    `;
+};
